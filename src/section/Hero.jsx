c052b48e128bb1774c7cc348a1704c9e7b8cef07
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-18 21:09:53 +0000

    fourth

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-import { useMaskSettings } from '../../constants';
+import { useMaskSettings } from '../../constent';
 import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
