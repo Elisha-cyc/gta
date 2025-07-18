e3c28647710a86eecd47cdb05a3c7072eb6c1472
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-18 21:05:37 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+
 import NavBar from './section/NavBar';
 import Hero from './section/Hero';
+import ComingSoon from './section/ComingSoon';
+import FirstVideo from './section/FirstVideo';
+
 gsap.registerPlugin(ScrollTrigger);
+
 const App = () => {
   return (
     <main>
-      <NavBar/>
-      <Hero/>
+      <NavBar />
+      <Hero />
+      <ComingSoon />
+      <FirstVideo />
+
     </main>
   )
 }
 
-export default App
+export default App;
