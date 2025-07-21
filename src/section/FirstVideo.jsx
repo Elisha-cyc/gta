2016ab456eacf2965db29bbc9c3f01054c7339e1
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-21 11:55:32 +0000

    fourth comment

--- src/section/FirstVideo.jsx
+++ src/section/FirstVideo.jsx
@@ -1,41 +1,44 @@
-import gsap from 'gsap'
-import {useGSAP} from '@gsap/react';
-import {useRef} from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useRef } from "react"
 
-const FirstVideo=()=> {
-    const videoRef = useRef(null);
-    useGSAP (()=> {
-        gsap.set('.first-vd-wrapper', {marginTop:'-150vh', opacity:0});
-        const tl=gsap.timeline({
-            scrollTrigger:{
-                trigger:'.first-vd-wrapper',
-                start:'top top',
-                end:'+=200% top',
-                scrub:true,
-                pin: true,
-            }
-        })
-        tl.to('.hero-section', {delay:0.5, opacity:0, ease: 'power2.inOut'});
-        tl.to('first-vd-wrapper', {opacity:1, duration:2, ease: 'power1.inOut'});
+const FirstVideo = () => {
+  const videoRef = useRef(null);
 
-        videoRef.current.onloadedmetadata=()=>{
-            tl.to(video.Ref.current,{currentTime: videoRef.current.duration, duration:3,ease:'power1.inOut'}, '<');
-        }
-    }, [])
+  useGSAP(() => {
+    gsap.set('.first-vd-wrapper', { marginTop: '-150vh', opacity: 0 });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.first-vd-wrapper',
+        start: 'top top',
+        end: '+=200% top',
+        scrub: true,
+        pin: true,
+      }
+    })
+
+    tl.to('.hero-section', { delay: 0.5, opacity: 0, ease: 'power1.inOut' });
+    tl.to('.first-vd-wrapper', { opacity: 1, duration: 2, ease: 'power1.inOut' });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, { currentTime: videoRef.current.duration, duration: 3, ease: 'power1.inOut' }, '<');
+    }
+  }, []);
+
   return (
-   <section className='first -vd-wrapper'>
-    <div className="h-dvh">
+    <section className="first-vd-wrapper">
+      <div className="h-dvh">
         <video 
-        ref={videoRef}
-        muted
-        playsInline
-        preload="auto"
-        src='/videos/output1.mp4'
-        className='first-vd'
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output1.mp4"
+          className="first-vd"
         />
-    </div>
-
-   </section>
+      </div>
+    </section>
   )
 }
 
--- src/section/NavBar.jsx
+++ src/section/NavBar.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-
 const NavBar = () => {
-    return (
-        <div>
-            <nav>
-                <img src="/images/nav-logo.svg" className='scale-90' />
-                <img src="/images/menu.svg" className='w-10' />
-            </nav>
-        </div>
-    )
+  return (
+    <nav>
+        <img src="/images/nav-logo.svg" className="scale-90" />
+        <img src="/images/menu.svg" className="w-10" />
+      </nav>
+  )
 }
 
 export default NavBar
